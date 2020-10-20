fbd41f10cbd0b480c637ff5a3dbeaa533a6ec879
Attia Nidhal <attianidhal@gmail.com>
2020-10-20 23:12:31 +0100

    correction bug

--- src/main/java/tn/esprit/spring/controller/IControllerEmployeImpl.java
+++ src/main/java/tn/esprit/spring/controller/IControllerEmployeImpl.java
@@ -8,37 +8,31 @@ import org.springframework.stereotype.Controller;
 
 import tn.esprit.spring.entities.Contrat;
 import tn.esprit.spring.entities.Employe;
-<<<<<<< HEAD
 import tn.esprit.spring.entities.Mission;
 import tn.esprit.spring.entities.Timesheet;
-import tn.esprit.spring.services.IEmployeService;
-=======
 import tn.esprit.spring.entities.Entreprise;
-import tn.esprit.spring.entities.Mission;
-import tn.esprit.spring.entities.Timesheet;
-import tn.esprit.spring.services.IEmployeService;
-import tn.esprit.spring.services.IEntrepriseService;
->>>>>>> refs/remotes/origin/main
-import tn.esprit.spring.services.ITimesheetService;
+import tn.esprit.spring.entities.Departement;
 
+import tn.esprit.spring.services.IEmployeService;
+import tn.esprit.spring.services.ITimesheetService;
 
 @Controller
 public class IControllerEmployeImpl  {
 	@Autowired
 	IEmployeService iemployeservice;
-<<<<<<< HEAD
+
 
 	@Autowired
 	ITimesheetService itimesheetservice;
 
 	
-=======
+
 	@Autowired
 	IEntrepriseService ientrepriseservice;
 	@Autowired
 	ITimesheetService itimesheetservice;
 
->>>>>>> refs/remotes/origin/main
+
 	public int ajouterEmploye(Employe employe)
 	{
 		iemployeservice.ajouterEmploye(employe);
@@ -100,14 +94,9 @@ public class IControllerEmployeImpl  {
 	}
 
 	
-<<<<<<< HEAD
-
-	
-=======
 	public List<Employe> getAllEmployeByEntreprise(Entreprise entreprise) {
 		return iemployeservice.getAllEmployeByEntreprise(entreprise);
 	}
->>>>>>> refs/remotes/origin/main
 
 
 	public void mettreAjourEmailByEmployeIdJPQL(String email, int employeId) {	
