843482fa566bdfce631c3cae7b1efbeafd6f44cb
Attia Nidhal <attianidhal@gmail.com>
2020-10-20 19:28:33 +0100

    bug fixed

--- src/main/java/tn/esprit/spring/repository/EmployeRepository.java
+++ src/main/java/tn/esprit/spring/repository/EmployeRepository.java
@@ -36,10 +36,12 @@ public interface EmployeRepository extends CrudRepository<Employe, Integer>  {
     @Query("select c.salaire from Contrat c join c.employe e where e.id=:employeId")
     public float getSalaireByEmployeIdJPQL(@Param("employeId")int employeId);
     
-    
-    
-	
-    		
+    @Query("Select "
+			+ "DISTINCT AVG(cont.salaire) from Contrat cont "
+			+ "join cont.employe emp "
+			+ "join emp.departements deps "
+			+ "where deps.id=:depId")
+    public Double getSalaireMoyenByDepartementId(@Param("depId")int departementId);		
    
 
 }
