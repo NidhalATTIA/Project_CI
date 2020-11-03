f9b4a44cd393be2a71742a33bfbffc6fd194f0fa
oumayma11 <oumayma.homrani@esprit.tn>
2020-11-03 23:42:52 +0100

    test

--- src/test/java/tn/junit/test/ContratTest.java
+++ src/test/java/tn/junit/test/ContratTest.java
@@ -1,45 +1,57 @@
 package tn.junit.test;
 
-/*import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import tn.esprit.spring.TimesheetSpringBootCoreDataJpaMvcRest1Application;
+
+import org.junit.Test;
 import static org.junit.Assert.assertNotNull;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import tn.esprit.spring.entities.Contrat;
 import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Mission;
 import tn.esprit.spring.services.EmployeServiceImpl;
-*/
+import tn.esprit.spring.services.IEmployeService;
+import tn.esprit.spring.services.ITimesheetService;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TimesheetSpringBootCoreDataJpaMvcRest1Application.class)
 public class ContratTest {
+	//oumayma
+	@Autowired 
+	IEmployeService es;
+	ITimesheetService ts;
 	
+	@Test
+	public void testAjoutContrat() throws ParseException {
+		SimpleDateFormat f=new SimpleDateFormat("dd/mm/yyyy");
+
+		Date d=f.parse("20/11/2001");
+		Contrat c=new Contrat(d,"work",1254f);
+		es.ajouterContrat(c);
+		assertNotNull(es.ajouterContrat(c));
+	}
 	
-	/*EmployeServiceImpl employeService=new EmployeServiceImpl();
-	Employe employe = new Employe();
-	Contrat contrat = new Contrat();
-	Mission mission = new Mission();
-	Date dateDebut;
-	Date dateFin;
-	int employeId,depId;
 	@Test
-	public void testAjoutContrat() {
-		assertNotNull(employeService.ajouterContrat(contrat));
+	public void testAffecterContratEmploye() throws ParseException {
+		es.affecterContratAEmploye(1, 1);
 	}
+	
 	@Test
-	public void testAjoutEmploye() {
-		assertNotNull(employeService.ajouterEmploye(employe));
+	public void testDeleteContart() throws ParseException {
+		es.deleteContratById(1);
 	}
-	@Test
-	public void testAjoutTimesheet() {
-		assertNotNull(employeService.getTimesheetsByMissionAndDate(employe,mission,dateDebut,dateFin));
-	}*/
 
-	/*@Test
-	public void testaffecterEmployeADepartement() {
-		assertNotNull(employeService.affecterEmployeADepartement(employeId,depId));
+	
 
-	}
-	*/
+	
 
 	
 }
