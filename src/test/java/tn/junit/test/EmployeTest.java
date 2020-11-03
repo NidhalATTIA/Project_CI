edd465878fda89a84ec376a365ea4835a59ec7f8
sahar <sahar.mansouri@esprit.tn>
2020-11-03 23:09:06 +0100

    test

--- src/test/java/tn/junit/test/EmployeTest.java
+++ src/test/java/tn/junit/test/EmployeTest.java
@@ -1,6 +1,5 @@
 package tn.junit.test;
 
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -18,39 +17,35 @@ import tn.esprit.spring.services.IEmployeService;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TimesheetSpringBootCoreDataJpaMvcRest1Application.class)
 public class EmployeTest {
-	@Autowired 
+	@Autowired
 	IEmployeService es;
 
-
-	//raya
+	// raya
 	@Test
-	public void testAjoutEmploye() {  
-		
-		//ajout Employe:
-	Employe employe1=new Employe("khaled","Kalel","khaled.kalel@ssiiconsulting.tn",true,Role.INGENIEUR);
-	es.ajouterEmploye(employe1);
-	assertNotNull(es.ajouterEmploye(employe1));
-	
-	Employe employe2=new Employe("Mouhamed","Zitouni","mohamed.zitouni@ssiiconsulting.tn",false,Role.TECHNICIEN);
-	es.ajouterEmploye(employe2);
-	assertNotNull(es.ajouterEmploye(employe2));
-	
-	Employe employe3=new Employe("Aymen","ouali","aymen.ouali@ssiiconsulting.tn",true,Role.INGENIEUR);
-	es.ajouterEmploye(employe3);
-	assertNotNull(es.ajouterEmploye(employe3));
-	
-	Employe employe4=new Employe("bochra","bouzid","bochra.bouzid@ssiiconsulting.tn",true,Role.CHEF_DEPARTEMENT);
-	es.ajouterEmploye(employe4);
-	assertNotNull(es.ajouterEmploye(employe4));
-	
-	Employe employe5=new Employe("yosra","arbi","yosra.arbi@ssiiconsulting.tn",true,Role.CHEF_DEPARTEMENT);
-	es.ajouterEmploye(employe5);
-	assertNotNull(es.ajouterEmploye(employe5));
-	
-			
+	public void testAjoutEmploye() {
+
+		// ajout Employe:
+		Employe employe1 = new Employe("khaled", "Kalel", "khaled.kalel@ssiiconsulting.tn", true, Role.INGENIEUR);
+		es.ajouterEmploye(employe1);
+		assertNotNull(es.ajouterEmploye(employe1));
+
+		Employe employe2 = new Employe("Mouhamed", "Zitouni", "mohamed.zitouni@ssiiconsulting.tn", false,
+				Role.TECHNICIEN);
+		es.ajouterEmploye(employe2);
+		assertNotNull(es.ajouterEmploye(employe2));
+
+		Employe employe3 = new Employe("Aymen", "ouali", "aymen.ouali@ssiiconsulting.tn", true, Role.INGENIEUR);
+		es.ajouterEmploye(employe3);
+		assertNotNull(es.ajouterEmploye(employe3));
+
+		Employe employe4 = new Employe("bochra", "bouzid", "bochra.bouzid@ssiiconsulting.tn", true,
+				Role.CHEF_DEPARTEMENT);
+		es.ajouterEmploye(employe4);
+		assertNotNull(es.ajouterEmploye(employe4));
+
+		Employe employe5 = new Employe("yosra", "arbi", "yosra.arbi@ssiiconsulting.tn", true, Role.CHEF_DEPARTEMENT);
+		es.ajouterEmploye(employe5);
+		assertNotNull(es.ajouterEmploye(employe5));
+
 	}
-	}
-
-	
-
-
+}
