918d87f192e1523a174b1c0aa34256d5f0c8ebd3
oumayma11 <oumayma.homrani@esprit.tn>
2020-11-03 20:49:26 +0100

    new test class

---
+++ src/test/java/tn/junit/test/ContratTest.java
@@ -0,0 +1,46 @@
+package tn.junit.test;
+
+/*import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Date;
+
+import tn.esprit.spring.entities.Contrat;
+import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.entities.Mission;
+import tn.esprit.spring.services.EmployeServiceImpl;
+*/
+
+public class ContratTest {
+	
+	
+	/*EmployeServiceImpl employeService=new EmployeServiceImpl();
+	Employe employe = new Employe();
+	Contrat contrat = new Contrat();
+	Mission mission = new Mission();
+	Date dateDebut;
+	Date dateFin;
+	int employeId,depId;
+	@Test
+	public void testAjoutContrat() {
+		assertNotNull(employeService.ajouterContrat(contrat));
+	}
+	@Test
+	public void testAjoutEmploye() {
+		assertNotNull(employeService.ajouterEmploye(employe));
+	}
+	@Test
+	public void testAjoutTimesheet() {
+		assertNotNull(employeService.getTimesheetsByMissionAndDate(employe,mission,dateDebut,dateFin));
+	}*/
+
+	/*@Test
+	public void testaffecterEmployeADepartement() {
+		assertNotNull(employeService.affecterEmployeADepartement(employeId,depId));
+
+	}
+	*/
+
+	
+}
+
