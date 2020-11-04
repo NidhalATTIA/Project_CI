86e20e67d700f0b40f4a5620579332e9f3b45d87
sahar <sahar.mansouri@esprit.tn>
2020-11-05 00:39:47 +0100

    test

--- src/test/java/tn/junit/test/EntrepriseTest.java
+++ src/test/java/tn/junit/test/EntrepriseTest.java
@@ -31,8 +31,8 @@ public class EntrepriseTest {
 
 	@Test
 	public void testGetEntrepriseById() {
-		es.getEntrepriseById(1);
-		assertNotNull(es.getEntrepriseById(1));
+		es.getEntrepriseById(39);
+		assertNotNull(es.getEntrepriseById(39));
 	}
 
 	@Test
