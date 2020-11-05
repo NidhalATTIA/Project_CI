4e68738eeff9c29a96c44008802b54b97b0c6765
oumayma11 <oumayma.homrani@esprit.tn>
2020-11-05 13:31:39 +0100

    test

--- src/test/java/tn/junit/test/ContratTest.java
+++ src/test/java/tn/junit/test/ContratTest.java
@@ -24,7 +24,7 @@ import tn.esprit.spring.services.ITimesheetService;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TimesheetSpringBootCoreDataJpaMvcRest1Application.class)
 public class ContratTest {
-	//oumaymaH
+	//oumayma
 	@Autowired 
 	IEmployeService es;
 	ITimesheetService ts;
